89e5198cecf2e93b2caf3a69dce3924ef44099b5
grechka00041 <23052006z@gmail.com>
2026-05-01 19:15:20 +0300

    Project setup

---
+++ src/main/java/com/grechka/eventticketplatform/EventTicetPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.grechka.eventticketplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicetPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicetPlatformApplication.class, args);
+    }
+
+}
